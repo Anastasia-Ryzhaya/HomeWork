1f8b1e98397ccf6e75d548d5ccb88632056fe525
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-03 23:29:03 +0500

    Решили 15 задачу

--- HomeWork02/Task15/Task15.cs
+++ HomeWork02/Task15/Task15.cs
@@ -1,2 +1,22 @@
 ﻿// 15. Напишите метод, который принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
+void isWeekend(int num)
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+    else if (num > 0 && num < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+    else
+    {
+        Console.WriteLine("Неверный формат данных");
+    }
+}
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
+
+isWeekend(day);
