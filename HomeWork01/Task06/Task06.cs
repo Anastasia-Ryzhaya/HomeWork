e317d1e0841b587fce374da7ece6307efce2beba
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-03 22:18:45 +0500

    Решили 6 и 8 задачи

---
+++ HomeWork01/Task06/Task06.cs
@@ -0,0 +1,19 @@
+﻿//  6. Выяснить является ли число чётным
+
+void isEven(int num)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine("Число четное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+isEven(number);
+
---
+++ HomeWork01/Task08/Task08.cs
@@ -0,0 +1,14 @@
+﻿// 8. Показать четные числа от 1 до N
+
+void printEven(int num)
+{
+    for (int numEven = 2; numEven <= num; numEven += 2)
+    {
+        Console.WriteLine(numEven);
+    }
+}
+
+Console.Write("Введите чило: ");
+int number = int.Parse(Console.ReadLine());
+
+printEven(number);
