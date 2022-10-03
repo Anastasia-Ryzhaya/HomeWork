3d60ffc38d1be6e55bd49a87ef5b9b774a853ca7
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-03 23:07:20 +0500

    Решили 10 задачу

---
+++ HomeWork02/Task10/Task10.cs
@@ -0,0 +1,12 @@
+﻿// 10. Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int secondDigit(int num)
+{
+    num = num / 10 % 10;
+    return num;
+}
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Втарая цифра веденного числа: {secondDigit(number)}");
