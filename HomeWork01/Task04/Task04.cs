f19419ec1d7c1760e47af63a7351f9363b3f48b0
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-03 22:44:40 +0500

    Решили 4 задачу

--- HomeWork01/Task04/Task04.cs
+++ HomeWork01/Task04/Task04.cs
@@ -1,3 +1,31 @@
 ﻿// 4. Найти максимальное из трех чисел
 
+int inputRequest()
+{
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int findMax(int num1, int num2, int num3)
+{
+    int max = num1;
+    if (num2 > max) 
+    {
+        max = num2;
+    }
+    if (num3 > max)
+    {
+        max = num3;
+    }
+    return max;
+}
+
+Console.WriteLine("Требуется ввести три числа.");
+
+int a = inputRequest();
+int b = inputRequest();
+int c = inputRequest();
+
+Console.WriteLine($"Максимальное число: {findMax(a, b, c)}");
 
