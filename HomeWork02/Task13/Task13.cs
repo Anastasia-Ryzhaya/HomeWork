8579eca938256817ced2bb6a7dca16acdd2e7e7a
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-04 19:44:23 +0500

    Решили 13 задачу

--- HomeWork02/Task13/Task13.cs
+++ HomeWork02/Task13/Task13.cs
@@ -1,11 +1,22 @@
 ﻿// 13. Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-void plusThirdDigit(int num)
+void thirdDigit(int num)
 {
-
+    if (Math.Abs(num) < 100)
+    {
+        Console.WriteLine("Третья цифра отсутствует");
+    }
+    else
+    {
+        while (Math.Abs(num) > 999)
+        {
+            num /= 10;
+        }
+        Console.WriteLine($"Третья цифра цисла: {Math.Abs(num % 10)}");
+    }
 }
 
-void minusThirdDigit(int num)
-{
-    
-}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+thirdDigit(number);
