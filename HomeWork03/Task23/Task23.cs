c0726ce0251350329472d4b5fa64570d3c107314
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-09 12:18:01 +0500

    Решили 23 задачу

--- HomeWork03/Task23/Task23.cs
+++ HomeWork03/Task23/Task23.cs
@@ -1,1 +1,19 @@
 ﻿// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int input()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void tabSqrt(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index}^2 = {Math.Pow(index, 2)}");
+        index++;
+    }
+}
+
+tabSqrt(input());
