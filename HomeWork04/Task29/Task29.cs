35789a7854ece667de4029307a63ef7149f3c2b7
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-10 22:36:09 +0500

    Решила 29 задачу

---
+++ HomeWork04/Task29/Task29.cs
@@ -0,0 +1,19 @@
+﻿// 29. Напишите метод, который задаёт массив из 8 элементов и выводит их на экран.
+
+int[] fillArray()
+{
+    int[] array = new int[8];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+
+string printArray(int[] mass)
+{
+    return String.Join(", ", mass);
+}
+
+Console.WriteLine($"[{printArray(fillArray())}]");
