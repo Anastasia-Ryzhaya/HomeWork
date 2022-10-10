6822d3b67381d622853dc76fd18ac4797bc107a0
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-10 21:20:04 +0500

    Решили 27 задачу

---
+++ HomeWork04/Task27/Task27.cs
@@ -0,0 +1,21 @@
+﻿// 27. Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+long input()
+{
+    Console.Write("Введите число: ");
+    long number = Convert.ToInt64(Console.ReadLine());
+    return number;
+}
+
+int sumDigit(long num)
+{
+    long result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return Convert.ToInt32(result); 
+}
+
+Console.WriteLine(sumDigit(input()));
