8f32709203ad0fc4c2b08f72a5bbf6f5d8b927a6
AnastasiaRyzhaya <ustyugovanastya@mail.ru>
2022-10-10 21:04:05 +0500

    Решили 25 задачу

---
+++ HomeWork04/Task25/Task25.cs
@@ -0,0 +1,34 @@
+﻿// 25. Опишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+
+void input(out int a, out int b)
+{
+    Console.Write("Введите число: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите степень: ");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+
+double exponentiation(double a, double b)
+{
+    double result = 1;
+    for (int i = 0; i < Math.Abs(b); i++)
+    {
+        result *= a;
+    }
+    if (b >= 0) 
+    {
+        return result;
+    }
+    else 
+    {
+        return 1 / result;
+    }   
+}
+
+int num1;
+int num2;
+
+input(out num1, out num2);
+
+Console.WriteLine(exponentiation(num1, num2));
